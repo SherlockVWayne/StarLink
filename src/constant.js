3a1f43c1311948fbce9a87672ffc33f6f98e5b15
SherlockVWayne <haoxiang@6554@gmail.com>
2020-08-26 08:54:07 -0700

    Final version

---
+++ src/constant.js
@@ -0,0 +1,11 @@
+export const SAT_BASE_URL = "https://www.n2yo.com/rest/v1/satellite";
+
+export const SAT_API_KEY = "9L2EJB-L9XGAE-72A5JK-4J14"; // Don’t copy this, use the API license key you get from N2YO
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = `${SAT_BASE_URL}/above`;
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+
+export const SATELLITE_POSITION_URL = `${SAT_BASE_URL}/positions`;
